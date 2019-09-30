237582ef3d41b78d3d0f8c316156d1a713503a2b
unknown <jasoncollum@gmail.com>
2019-09-30 08:35:00 -0700

    setup Filter Entries link toggle to show/hide Filter Buttons

--- bpm/wwwroot/js/site.js
+++ bpm/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const filtersDiv = document.querySelectorAll('.filters-div');
+filtersDiv.forEach()
